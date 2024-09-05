96868fd5726e28d723f5f1c5f5c0d66ea8e5ff4a
rhodew <rhodew@udel.edu>
2024-09-05 08:33:35 -0700

    says other stuff too

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,6 +8,14 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World!");
+        Debug.LogWarning("We are all going to die someday.");
+        Debug.LogError("I was unable to decipher your awful code.");
+        //Author: Rhode
+        /*
+         * How much codinng am I going to learn here?
+         * I would like to make some kind of zelda-like horror game at some point.
+         * I would like to learn more about how to make games work.
+        */
     }
 
     // Update is called once per frame
