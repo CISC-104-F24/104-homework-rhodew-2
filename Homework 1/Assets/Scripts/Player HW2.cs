a2febd476f708cc9a58d62dedf31371b394e268f
rhodew <rhodew@udel.edu>
2024-10-31 12:11:02 -0700

    HW3

--- Homework 1/Assets/Scripts/Player HW2.cs
+++ Homework 1/Assets/Scripts/Player HW2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class PlayerHW2 : MonoBehaviour
@@ -12,9 +13,11 @@ public class PlayerHW2 : MonoBehaviour
     public float moveSpeed = 1.0f;
     public float jumpPower = 1.0f;
     public float chargeSpeed = 1.0f;
+    public float sand = 1.0f;
     float totalJump;
     public float sprintSpeed = 1.0f;
     float Sprint;
+    public Vector3 startingPlayerPosition;
     Vector3 Forward = new Vector3(0f, 0f, 1f);
     Vector3 Backward = new Vector3(0f, 0f, -1f);
     Vector3 Right = new Vector3(1f, 0f, 0f);
@@ -63,7 +66,7 @@ public class PlayerHW2 : MonoBehaviour
         if (isSpacePressed == true)
         {
             Rigidbody myRigidbody = GetComponent<Rigidbody>();
-            myRigidbody.AddForce(Up * totalJump, ForceMode.Impulse);
+            myRigidbody.AddForce(Up * totalJump * sand, ForceMode.Impulse);
             totalJump = jumpPower;
         }
         if (isShiftPressed == true)
@@ -86,9 +89,72 @@ public class PlayerHW2 : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
-        transform.position = transform.position + moveDirection * moveSpeed * Sprint * Time.deltaTime;
+        transform.position = transform.position + moveDirection * moveSpeed * Sprint * Time.deltaTime * sand;
+    }
 
+        private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Teleporter"))
+        {
+            transform.position = new Vector3(7f, 19f, -11f);
+        }
+        if (collision.gameObject.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("why?");
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Sand"))
+        {
+            sand = 0.4f;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Boing"))
+        {
+            Rigidbody myRigidbody = GetComponent<Rigidbody>();
+            myRigidbody.AddForce(Up * 10, ForceMode.Impulse);
+        }
+        if (collision.gameObject.CompareTag("Sand"))
+        {
+            sand = 1.0f;
+        }
+    }
 
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrier"))
+            {
+                transform.position = startingPlayerPosition;
+                Debug.LogWarning("There is no escape");
+            }
+        if (other.gameObject.CompareTag("PersonalSpace"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+       if (other.gameObject.CompareTag("NoPass"))
+        {
+            Rigidbody myRigidbody = GetComponent<Rigidbody>();
+            myRigidbody.AddForce(Right * 3, ForceMode.Impulse);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bubble"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
+
